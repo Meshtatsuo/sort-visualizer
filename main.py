faf1d20e07cccf13412904451af90fca8898398f
Dominic Misasi <meshtatsuo@gmail.com>
2022-06-18 05:32:48 -0600

    added timer

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import sys
+import time
+from tracemalloc import start
 
 from algorithms.bubble import *
 from algorithms.insertion import *
 
 dataSize = int(sys.argv[2])
-playbackSpeed = .001
+playbackSpeed = .1
 sortType = str(sys.argv[1])
 sortType = sortType.lower()
-print(sortType)
 
 list= np.random.randint(0, 100, dataSize)
 x = np.arange(0,dataSize,1)
 
+def calculateAndDisplayTime():
+    elapsed = endTime - startTime
+    
+    return
+
+
+def beginSort():
+    # bubble sort
+    if sortType == "bubble":
+        startTime = time.time()
+        bubbleSort(x,list, playbackSpeed)
+        endTime = time.time()
+        duration = endTime - startTime
+        return duration
+    if sortType == "insertion":
+        startTime = time.time()
+        insertionSort(x, list, playbackSpeed)
+        endTime = time.time()
+        duration = endTime - startTime
+        return duration
+    else:
+        print("Please enter a valid sort algorithm, We currently support:")
+        print("- bubble")
+        return
+    
+
 print("Launching visualizer for sorting with type: ", sortType)
-
-# bubble sort
-if sortType == "bubble":
-    bubbleSort(x,list, playbackSpeed)
-
-if sortType == "insertion":
-    insertionSort(x, list, playbackSpeed)
-    
-else:
-    print("Please enter a valid sort algorithm, We currently support:")
-    print("- bubble")
-    
-
+elapsed = beginSort()
 print("Finished!")
+print("Sorting using ", sortType, " took: ", elapsed, " seconds.")
 plot.show()
